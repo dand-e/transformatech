8e9bd0c81df3991f1736c9a468db4824b6c791b1
DanVi <danicrisa@hotmail.com>
2022-05-04 02:38:20 -0300

    adding DOM

---
+++ javascript/DOM.js
@@ -0,0 +1,11 @@
+var myElement = document.getElementsById('header');
+var myElement = document.getElementsByClassName('container');
+var myElement = document.getElementsByTagName('h1');
+var myElement = document.querySelector('#btn');
+var myElement = document.querySelectorAll('#div_id');
+
+myBtn.addEventListener('select', function(event) {
+
+  alert('Elemento selecionado!');
+
+}, false);
